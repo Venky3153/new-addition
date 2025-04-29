afbfba2a6bd9e0add203f48cc0f88f4ad340e4c5
Venky3153 <venkysmart315@gmail.com>
2025-04-29 16:00:39 +0530

    Add files via upload

---
+++ calculator2.java
@@ -0,0 +1,22 @@
+public class calculator{
+
+public static void addition(int a ,int b){
+int c =a +b;
+System.out.println("addition value="+c);
+
+}
+public static void division(int d ,int e){
+int t =d/e;
+System.out.println("division value="+t);
+
+
+}
+
+public static void main(String[] args){
+ addition(2,3);
+division(10,2);
+}
+
+
+
+}
